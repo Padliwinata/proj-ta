bc966f09ed2aeb5cf436d50e7e9696c8a1e7e7c0
Padliwinata <rpadliwinata@gmail.com>
2024-02-04 07:41:11 +0700

    update

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from deta import Deta
 
 app = FastAPI()
+deta = Deta("c0YK3LGkSzAM_WPS5wNp7NafechQa2id7Q7Sd9Z8UaxmZ")
+db_user = deta.Base("user")
 
 
-@app.get('/')
+@app.get('/put')
 async def index():
-    return {"message": "success"}
+    db_user.put({'username': 'example'})
+    return {"username": "example"}
+
+@app.get('/get')
+async def get_data():
+    res = db_user.fetch()
+    return {"data": res.items}
