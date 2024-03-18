6dbf9a51ff502aa2a5e667aff29fcee51a9da037
Padliwinata <rpadliwinata@gmail.com>
2024-03-18 11:50:29 +0700

    update

--- models.py
+++ models.py
@@ -1,47 +1,85 @@
 from enum import Enum
-from typing import Optional, Dict, Any
+import typing
 
 from pydantic import BaseModel, SecretStr, EmailStr
+
+from db import db_institution
 
 
 class UserRole(str, Enum):
     admin = "admin"
     super_admin = "super admin"
     staff = "staff"
+    reviewer = "reviewer"
+
+
+class Position(str, Enum):
+    pic = "pic"
+    owner = "owner"
 
 
 class RegisterForm(BaseModel):
     username: str
     email: EmailStr
     password: SecretStr
+    full_name: str
+    role: UserRole
+    phone: str
+    institution_name: str
+    institution_address: str
+    institution_phone: str
+    institution_email: EmailStr
+
+
+class Institution(BaseModel):
+    name: str
+    address: str
+    phone: str
+    email: EmailStr
 
 
 class Response(BaseModel):
     success: bool
     code: int
     message: str
-    data: Optional[Dict[str, Any]]
+    data: typing.Union[typing.Dict[str, typing.Any], typing.List[typing.Any], None]
 
 
 class ResponseDev(Response):
-    access_token: Optional[str]
+    access_token: typing.Optional[str]
 
 
 class User(BaseModel):
     username: str
+    password: typing.Union[SecretStr, bytes, None]
     email: str
-    password: Optional[SecretStr]
     role: UserRole = UserRole.admin
+    position: Position = Position.owner
+    id_institution: str
     is_active: bool
+
+    def get_institution(self) -> typing.Dict[str, typing.Any]:
+        institution = db_institution.get(self.id_institution)
+        data = Institution(**institution)
+        return data.dict()
 
 
 class AddUser(BaseModel):
     username: str
     email: str
-    password: Optional[SecretStr]
+    password: typing.Optional[SecretStr]
     role: UserRole = UserRole.admin
 
 
 class Refresh(BaseModel):
     refresh_token: str
+
+
+class Payload(BaseModel):
+    sub: str
+    role: str
+    iat: int
+
+
+
 
