4864d5187f67db2bea134a38763606fe8853ec4c
amaliayunaa <42332019+amaliayunaa@users.noreply.github.com>
2024-03-19 09:42:10 +0700

    tidak jelas ngapain awokwkw

--- test.py
+++ test.py
@@ -5,25 +5,21 @@ from db import db_user
 
 
 client = TestClient(app)
-
+    
 
 def test_register_user() -> None:
-    # Test case for user registration
+    # Test case for successful user registration
     test_data = {
         'username': 'testing_username',
         'email': 'testing@gmail.com',
         'password': 'testing_password'
     }
-
-    # Test with valid data
     response = client.post('/register', json=test_data)
-    if response.status_code == 200 and response.json()['success'] is True:
-        print("Registration successful.")
-        client.delete('/test')
-    else:
-        print("Invalid data. Registration failed.")
+    assert response.status_code == 200
+    assert response.json()['success'] is True
 
-    # Test with invalid data
+
+
 def test_register_invalid_data() -> None:
     # Test case for registering a user with invalid data
     invalid_user_data = {
@@ -32,12 +28,10 @@ def test_register_invalid_data() -> None:
         'password': 'paswordyuna' 
     }
     response = client.post('/register', json=invalid_user_data)
+    assert response.status_code == 422  
+    assert 'detail' in response.json()
+    assert 'value is not a valid email address' in response.json()['detail'][0]['msg']
     
-    if response.status_code == 422 and 'detail' in response.json() and 'validation error' in response.json()['detail'][0]['msg']:
-        print("Email not valid.")
-    else:
-        print("registration successful.")
-
 def test_login_user() -> None:
     # Test case for user login
     test_data = {
@@ -47,29 +41,8 @@ def test_login_user() -> None:
 
     # Test with valid login credentials
     response = client.post('/login', json=test_data)
-    if response.status_code == 200 and response.json()['success'] is True:
-        print("Login successful.")
-    else:
-        print("Invalid login credentials. Login failed.")
-
-    # Test with invalid login credentials
-def test_login_invalid_credentials() -> None:
-    # Test case for login with invalid credentials
-    invalid_login_data = {
-        'username': 'invalid_username',
-        'password': 'invalid_password'
-    }
-
-    response = client.post('/login', json=invalid_login_data)
-    
-    if response.status_code == 401 and 'detail' in response.json() and 'Invalid credentials' in response.json()['detail']:
-        print("Invalid login credentials.")
-       
-    else:
-        print("Login successful.")
-        
+    assert response.status_code == 200 and response.json()['success'] is True
+    # Additional actions for successful login, if needed
 
 
-
-    
 
