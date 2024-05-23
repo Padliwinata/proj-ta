680a87c9b307111b411dad6912ec4e287958bc1c
Padliwinata <rpadliwinata@gmail.com>
2024-05-24 01:15:18 +0700

    update

--- db.py
+++ db.py
@@ -13,13 +13,4 @@ db_log = deta_obj.Base("log")
 db_point = deta_obj.Base("point")
 db_assessment = deta_obj.Base("assessment")
 
-test_db_user = deta_obj.Base("test_user")
-test_db_institution = deta_obj.Base("test_institution")
-test_db_proof = deta_obj.Base("test_proof")
-test_db_questionnaire = deta_obj.Base("test_questionnaire")
-test_db_report = deta_obj.Base("test_report")
-test_db_log = deta_obj.Base("test_log")
-test_db_point = deta_obj.Base("test_point")
-test_db_assessment = deta_obj.Base("test_assessment")
-
 drive = deta_obj.Drive("document")
