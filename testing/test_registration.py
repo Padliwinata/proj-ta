52a5ab8abf0eac1163889fa3aa148c66d071ad47
amaliayunaa <42332019+amaliayunaa@users.noreply.github.com>
2024-06-11 19:17:52 +0700

    update test case registration dan login

--- testing/test_registration.py
+++ testing/test_registration.py
@@ -5,48 +5,61 @@ import json
 
 from fastapi.testclient import TestClient # type: ignore
 from main import app
+from main import register
+
 
 client = TestClient(app)
 
-def test_admin_registration():
-    response = client.post(
+import unittest
+class TestRegistration(unittest.TestCase):
+    def test_admin_registration(self): #test registrasi line 155
+        response = client.post(
         "/api/register",
         json={
-            "username": "admintesting2",
-            "email": "admin2@testing.com",
-            "password": "password",
-            "full_name": "Admin Testing",
+            "username": "admintesting5",
+            "email": "admin5@testing.com",
+            "password": "passwordd",
+            "full_name": "Admin Testing 5",
             "role": "admin",
-            "phone": "082991883774",
-            "institution_name": "Admin Institution",
-            "institution_address": "Jl. Pahlawan",
-            "institution_phone": "082993884775",
-            "institution_email": "admin@institution.com"
+            "phone": "0829918837744",
+            "institution_name": "Admin Institutionn",
+            "institution_address": "Jl. Pahlawann",
+            "institution_phone": "0829938847755",
+            "institution_email": "adminn@institution.com"
         }
     )
 
-    assert response.status_code == 201
-    assert response.json()["message"] == "User registered successfully"
-    assert response.json()["success"] is True
+        assert response.status_code == 201
+        assert response.json()["message"] == "User registered successfully"
+        assert response.json()["success"] is True
+        
+ 
 
-def test_admin_registration_account_exist():
-    response = client.post(
+    def test_admin_registration_account_exist(self):
+        response = client.post(
         "/api/register",
         json={
-            "username": "admintesting2",
-            "email": "admin2@testing.com",
-            "password": "password",
-            "full_name": "Admin Testing",
+            "username": "admintesting5",
+            "email": "admin5@testing.com",
+            "password": "passwordd",
+            "full_name": "Admin Testing 5",
             "role": "admin",
-            "phone": "082991883774",
-            "institution_name": "Admin Institution",
-            "institution_address": "Jl. Pahlawan",
-            "institution_phone": "082993884775",
-            "institution_email": "admin@institution.com"
+            "phone": "0829918837744",
+            "institution_name": "Admin Institutionn",
+            "institution_address": "Jl. Pahlawann",
+            "institution_phone": "0829938847755",
+            "institution_email": "adminn@institution.com"
         }
     )
+        
+           # Print the response for debugging
+        print(response.status_code)
+        print(response.json())
 
-    assert response.status_code == 400
-    assert response.json()["message"] == "Username or email already"
-    assert response.json()["success"] is False
+        assert response.status_code == 400
+        assert response.json()["message"] == "Username or email already "
+        assert response.json()["success"] is False
+
+if __name__ == "__main__":
+    unittest.main()
 
