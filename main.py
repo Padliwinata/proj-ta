5cf4b418079e28805698612ad01aa91a5c870be8
Padliwinata <rpadliwinata@gmail.com>
2024-06-29 17:16:49 +0700

    update

--- main.py
+++ main.py
@@ -65,18 +65,18 @@ router = APIRouter(prefix='/api')
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='api/auth', auto_error=False)
 f = Fernet(SECRET_KEY)
 
-# origins = [
-#     'http://127.0.0.1:3000',
-#     'http://127.0.0.1:8000',
-#     'https://devta-1-j8022502.deta.app'
-#     'https://fe-fraud.vercel.app',
-#     'https://www.frauddeterrence.online',
-#     'http://localhost:3000'
-# ]
+request_origins = [
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:8000',
+    'https://devta-1-j8022502.deta.app'
+    'https://fe-fraud.vercel.app',
+    'https://www.frauddeterrence.online',
+    'http://localhost:3000'
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=request_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
