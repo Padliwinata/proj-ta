60b2f1de44a9d1340b9fa0fe3fab56a72dc95c26
Padliwinata <rpadliwinata@gmail.com>
2024-07-10 15:59:19 +0700

    geser logger

--- main.py
+++ main.py
@@ -1174,8 +1174,8 @@ async def get_all_assessment(user: UserDB = Depends(get_user)) -> JSONResponse:
             status_code=status.HTTP_200_OK
         )
 
-    raw_data = [AssessmentDB(**x) for x in existing_assessments_data]
     print(existing_assessments_data)
+    raw_data = [AssessmentDB(**x) for x in existing_assessments_data]
     data = [assessment.get_all_dict() for assessment in raw_data]
 
     # print(existing_assessments_data)
