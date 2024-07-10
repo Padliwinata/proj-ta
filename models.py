664fe3797352bb8d54bf15dc9c0324bb69051d0c
Padliwinata <rpadliwinata@gmail.com>
2024-07-10 20:54:41 +0700

    fixed proof to id proof

--- models.py
+++ models.py
@@ -149,7 +149,7 @@ class Point(BaseModel):
     point: float
     answer: float
     skor: typing.Optional[float]
-    proof: typing.Union[Proof, str]
+    id_proof: typing.Union[Proof, str]
 
 
 class PointDB(Point):
