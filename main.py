52172dd76763fbcdcbaadae94fdf829632e4d9db
Padliwinata <rpadliwinata@gmail.com>
2024-07-10 13:41:38 +0700

    fixed get unfinished assessments

--- main.py
+++ main.py
@@ -624,7 +624,7 @@ async def upload_proof_point(request: Request,
         )
 
     # existing_assessment_data = db_assessment.fetch({'id_admin': user.data_key, 'selesai': False})
-    assessment_data = get_unfinished_assessments_by_institution(user.data_key)
+    assessment_data = get_unfinished_assessments_by_institution(user.id_institution)
     # print(assessment_data)
 
     if not assessment_data:
@@ -730,7 +730,7 @@ async def update_assessment(request: Request,
         )
 
     # existing_assessment_data = db_assessment.fetch({'id_admin': user.data_key, 'selesai': False})
-    existing_assessment_data = get_unfinished_assessments_by_institution(user.data_key)
+    existing_assessment_data = get_unfinished_assessments_by_institution(user.id_institution)
     if not existing_assessment_data:
         return create_response(
             message="No active assessment",
@@ -1002,7 +1002,7 @@ async def verify_user(userid: str) -> JSONResponse:
 @router.post("/assessment", tags=['Deterrence - Admin'])
 async def start_assessment(user: UserDB = Depends(get_user)) -> JSONResponse:
     # existing_data = db_assessment.fetch({'id_admin': user.data_key, 'selesai': False})
-    existing_data = get_unfinished_assessments_by_institution(user.data_key)
+    existing_data = get_unfinished_assessments_by_institution(user.id_institution)
     if existing_data:
         return create_response(
             message="Please finish last assessment first",
@@ -1039,7 +1039,7 @@ async def start_assessment(user: UserDB = Depends(get_user)) -> JSONResponse:
 @router.get("/assessment", tags=['Deterrence - Admin'])
 async def get_current_assessment(sub_bab: str, user: UserDB = Depends(get_user)) -> JSONResponse:
     # existing_assessment_data = db_assessment.fetch({'id_admin': user.data_key, 'selesai': False})
-    existing_assessment_data = get_unfinished_assessments_by_institution(user.data_key)
+    existing_assessment_data = get_unfinished_assessments_by_institution(user.id_institution)
     if not existing_assessment_data:
         return create_response(
             message="No active assessment",
@@ -1197,7 +1197,7 @@ async def get_finished_assessments(user: UserDB = Depends(get_user)) -> JSONResp
         )
 
     # existing_assessment = db_assessment.fetch({'id_admin': user.data_key, 'selesai': False})
-    existing_assessment = get_unfinished_assessments_by_institution(user.data_key)
+    existing_assessment = get_unfinished_assessments_by_institution(user.id_institution)
     if not existing_assessment:
         return create_response(
             message="Assessment not found",
