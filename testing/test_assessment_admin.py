1861e59693e4e1a5a8302ca4d54dbf2dc6de259d
amaliayunaa <42332019+amaliayunaa@users.noreply.github.com>
2024-07-11 10:27:30 +0700

    update test case assessment

--- testing/test_assessment_admin.py
+++ testing/test_assessment_admin.py
@@ -1,18 +1,22 @@
 import sys
 import os
 import unittest
+
+import pytest
 from fastapi.testclient import TestClient
 from main import app
-from html_reporter import HTMLTestRunner
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from db import get_proof_by_filename, delete_proof_by_key, delete_assessment
 
 
 client = TestClient(app)
 
 
 class TestAssessmentFDP(unittest.TestCase):
-    
-    def test_admin_unfinished_asessment(self):
+
+    @pytest.mark.order(1)
+    def test_admin_start_asessment_success(self):
+        # Pastikan last assessment harus sudah finish
         login_response = client.post(
             "/api/auth",
             data={"username": "adminperusahaan", "password": "admin"}
@@ -21,37 +25,18 @@ class TestAssessmentFDP(unittest.TestCase):
         access_token = login_response.json()["data"]["access_token"]
 
         response = client.post(
-            "/api/selesai",
-            headers={"Authorization": f"Bearer {access_token}"},
-            params={
-                "id_assessment": "n7v8yzxyhypz"
-            }
+            "/api/assessment",
+            headers={"Authorization": f"Bearer {access_token}"}
         )
+        # print(response.status_code)
+        # print(response.json())
+
         
-        assert response.status_code == 400
-        assert response.json()["message"] == "Unfinished assessment"
-        assert response.json()["success"] is False
-
-    def test_admin_finish_asessment(self):
-        login_response = client.post(
-            "/api/auth",
-            data={"username": "adminperusahaan", "password": "admin"}
-        )
-        assert login_response.status_code == 200
-        access_token = login_response.json()["data"]["access_token"]
-
-        response = client.post(
-            "/api/selesai",
-            headers={"Authorization": f"Bearer {access_token}"},
-            params={
-                "id_assessment": "05p9ut2hmkx4"
-            }
-        )
-
-        assert response.status_code == 200
-        assert response.json()["message"] == "Assessment finished"
+        assert response.status_code == 201
+        assert response.json()["message"] == "Start assessment success"
         assert response.json()["success"] is True
 
+    @pytest.mark.order(2)
     def test_admin_start_asessment_failed(self):
         login_response = client.post(
             "/api/auth",
@@ -65,29 +50,14 @@ class TestAssessmentFDP(unittest.TestCase):
             headers={"Authorization": f"Bearer {access_token}"},
             json={}
         )
+        # print(response.status_code)
+        # print(response.json())
         
         assert response.status_code == 400
         assert response.json()["message"] == "Please finish last assessment first"
         assert response.json()["success"] is False
 
-    def test_admin_start_asessment_success(self):
-        login_response = client.post(
-            "/api/auth",
-            data={"username": "adminperusahaan", "password": "admin"}
-        )
-        assert login_response.status_code == 200
-        access_token = login_response.json()["data"]["access_token"]
-
-        response = client.post(
-            "/api/assessment",
-            headers={"Authorization": f"Bearer {access_token}"},
-            json={}
-        )
-
-        assert response.status_code == 201
-        assert response.json()["message"] == "Start assessment success"
-        assert response.json()["success"] is True
-    
+    @pytest.mark.order(3)
     def test_admin_update_assesment_point_not_found(self):
         login_response = client.post(
             "/api/auth",
@@ -101,7 +71,7 @@ class TestAssessmentFDP(unittest.TestCase):
             headers={"Authorization": f"Bearer {access_token}"},
             params={
                 "bab": 10,
-                "sub_bab": 5.1,
+                "sub_bab": 5.0,
                 "point": 2,
                 "answer": 3,
             })
@@ -110,51 +80,8 @@ class TestAssessmentFDP(unittest.TestCase):
         assert response.json()["message"] == 'Point not found'
         assert response.json()["success"] is False
 
-    def test_admin_update_assesment_point_success(self):
-        login_response = client.post(
-            "/api/auth",
-            data={"username": "adminperusahaan", "password": "admin"}
-        )
-        assert login_response.status_code == 200
-        access_token = login_response.json()["data"]["access_token"]
-
-        response = client.patch(
-            "/api/point",
-            headers={"Authorization": f"Bearer {access_token}"},
-            params={
-                "bab": 11,
-                "sub_bab": 1.0,
-                "point": 1,
-                "answer": 2,
-            })
-
-        assert response.status_code == 200
-        assert response.json()["message"] == 'Update success'
-        assert response.json()["success"] is True
-
+    @pytest.mark.order(4)
     def test_admin_update_assesment_success(self):
-        login_response = client.post(
-            "/api/auth",
-            data={"username": "adminperusahaan", "password": "admin"}
-        )
-        assert login_response.status_code == 200
-        access_token = login_response.json()["data"]["access_token"]
-
-        response = client.patch(
-            "/api/point",
-            headers={"Authorization": f"Bearer {access_token}"},
-            params={
-                "bab": 11,
-                "sub_bab": 1.0,
-                "point": 1,
-                "answer": 2,
-            })
-
-        assert response.status_code == 200
-        assert response.json()["message"] == 'Update success'
-        assert response.json()["success"] is True
-
-    def test_admin_get_assesment_list(self):
         login_response = client.post(
             "/api/auth",
             data={"username": "adminperusahaan", "password": "admin"}
@@ -171,7 +98,8 @@ class TestAssessmentFDP(unittest.TestCase):
         assert response.json()["message"] == 'Success fetch data'
         assert response.json()["success"] is True
 
-    def test_admin_get_detail_grade_from_reviewer(self):
+    @pytest.mark.order(5)
+    def test_admin_get_assesment_list(self):
         login_response = client.post(
             "/api/auth",
             data={"username": "adminperusahaan", "password": "admin"}
@@ -179,41 +107,56 @@ class TestAssessmentFDP(unittest.TestCase):
         assert login_response.status_code == 200
         access_token = login_response.json()["data"]["access_token"]
 
-        assessment_id = '05p9ut2hmkx4'
         response = client.get(
-            f"/api/assessment/insight/{assessment_id}",
+            "/api/assessments",
             headers={"Authorization": f"Bearer {access_token}"}
         )
-        
+
+        client.delete('/api/dev/assessments')
+
         assert response.status_code == 200
-        assert response.json()["message"] == 'Fetch data success'
+        assert response.json()["message"] == 'Success fetch data'
         assert response.json()["success"] is True
 
-    def test_admin_get_detail_grade_from_reviewer_but_assignment_not_found(self):
-        login_response = client.post(
-            "/api/auth",
-            data={"username": "adminperusahaan", "password": "admin"}
-        )
-        assert login_response.status_code == 200
-        access_token = login_response.json()["data"]["access_token"]
+    # def test_admin_get_detail_grade_from_reviewer(self):
+    #     login_response = client.post(
+    #         "/api/auth",
+    #         data={"username": "adminperusahaan", "password": "admin"}
+    #     )
+    #     assert login_response.status_code == 200
+    #     access_token = login_response.json()["data"]["access_token"]
 
-        assessment_id = 'notfound'
-        response = client.get(
-            f"/api/assessment/insight/{assessment_id}",
-            headers={"Authorization": f"Bearer {access_token}"}
-        )
+    #     assessment_id = 'ev9ag3o7lxed'
+    #     response = client.get(
+    #         f"/api/assessment/insight{assessment_id}",
+    #         headers={"Authorization": f"Bearer {access_token}"}
+    #     )
+        
+    #     print(response.status_code)
+    #     print(response.json())
 
-        assert response.status_code == 404
-        assert response.json()["message"] == 'Assessment not found'
-        assert response.json()["success"] is False
+    #     assert response.status_code == 200
+    #     assert response.json()["message"] == 'Fetch data success'
+    #     assert response.json()["success"] is True
+
+    # def test_admin_get_detail_grade_from_reviewer_but_assignment_not_found(self):
+    #     login_response = client.post(
+    #         "/api/auth",
+    #         data={"username": "adminperusahaan", "password": "admin"}
+    #     )
+    #     assert login_response.status_code == 200
+    #     access_token = login_response.json()["data"]["access_token"]
+
+    #     assessment_id = 'notfound'
+    #     response = client.get(
+    #         f"/api/assessment/insight/{assessment_id}",
+    #         headers={"Authorization": f"Bearer {access_token}"}
+    #     )
+
+    #     assert response.status_code == 404
+    #     assert response.json()["message"] == 'Assessment not found'
+    #     assert response.json()["success"] is False
 
 
 if __name__ == "__main__":
-    runner = HTMLTestRunner(
-        report_filepath="my_report.html",
-        title="Test Assessment",
-        description="Ini test login",
-        open_in_browser=True
-    )
-
-    # run the test
+    unittest.main()
