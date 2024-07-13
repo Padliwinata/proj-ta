e37eba45e620d314cb6c9b1d1ae337f11c812b9b
Padliwinata <rpadliwinata@gmail.com>
2024-07-13 12:07:17 +0700

    logger

--- main.py
+++ main.py
@@ -1155,9 +1155,9 @@ async def get_assessment_insight(key: str, user: UserDB = Depends(get_user)) -> 
         points[sub_bab] = [point for point in existing_point if point['sub_bab'] == sub_bab]
 
     print(json.dumps(points, indent=4))
-    
+
     for key, value in points.items():
-        existing_skor = len([skor['skor'] for skor in value if isinstance(skor['skor'], float)])
+        existing_skor = len([skor['skor'] for skor in value])
         if existing_skor == question_number[bab.index(key)]:
             points[key] = sum([skor['skor'] for skor in value])
         else:
