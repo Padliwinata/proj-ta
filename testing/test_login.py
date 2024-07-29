fa231cd72115e3bdb0ece8f8251fd509345ef250
amaliayunaa <42332019+amaliayunaa@users.noreply.github.com>
2024-07-29 20:50:18 +0700

    uncoment

--- testing/test_login.py
+++ testing/test_login.py
@@ -10,26 +10,26 @@ client = TestClient(app)
 
 import unittest
 class TestLogin(unittest.TestCase):
-    # def remove_access_token(self):
-    #     return {"access_token": None}
+    def remove_access_token(self):
+        return {"access_token": None}
 
-    # def test_login_superadmin(self):
-    #     response = client.post(
-    #     "/api/auth",
-    #     data={"username": "username", "password": "password"}
-    # )
-    #     assert response.status_code == 200
-    #     assert response.json()["success"] is True
-    #     assert "access_token" in response.json()["data"]
+    def test_login_superadmin(self):
+        response = client.post(
+        "/api/auth",
+        data={"username": "username", "password": "password"}
+    )
+        assert response.status_code == 200
+        assert response.json()["success"] is True
+        assert "access_token" in response.json()["data"]
         
-    # def test_login_superadmin_failed(self):
-    #     response = client.post(
-    #     "/api/auth",
-    #     data={"username": "superadminnonexist", "password": "wrongpassword"}
-    # )
-    #     assert response.status_code == 401
-    #     assert response.json()["success"] is False
-    #     assert response.json()["message"] == "User not found"
+    def test_login_superadmin_failed(self):
+        response = client.post(
+        "/api/auth",
+        data={"username": "superadminnonexist", "password": "wrongpassword"}
+    )
+        assert response.status_code == 401
+        assert response.json()["success"] is False
+        assert response.json()["message"] == "User not found"
 
     def test_login_admin(self):
         response = client.post(
@@ -49,50 +49,50 @@ class TestLogin(unittest.TestCase):
         assert response.json()["success"] is False
         assert response.json()["message"] == "User not found"
 
-    # def test_login_staff(self):
-    #     response = client.post(
-    #     "/api/auth",
-    #     data={"username": "staff_perusahaan", "password": "password"}
-    # )
-    #     assert response.status_code == 200
-    #     assert response.json()["success"] is True
-    #     assert "access_token" in response.json()["data"]
+    def test_login_staff(self):
+        response = client.post(
+        "/api/auth",
+        data={"username": "staff_perusahaan", "password": "password"}
+    )
+        assert response.status_code == 200
+        assert response.json()["success"] is True
+        assert "access_token" in response.json()["data"]
 
-    # def test_login_reviewer_internal(self):
-    #     response = client.post(
-    #     "/api/auth",
-    #     data={"username": "emma_jones", "password": "password"}
-    # )
-    #     assert response.status_code == 200
-    #     assert response.json()["success"] is True
-    #     assert "access_token" in response.json()["data"]
+    def test_login_reviewer_internal(self):
+        response = client.post(
+        "/api/auth",
+        data={"username": "emma_jones", "password": "password"}
+    )
+        assert response.status_code == 200
+        assert response.json()["success"] is True
+        assert "access_token" in response.json()["data"]
 
-    # def test_login_reviewer_internal_failed(self):
-    #     response = client.post(
-    #     "/api/auth",
-    #     data={"username": "reviewerinternalnonexist", "password": "wrongpassword"}
-    # )
-    #     assert response.status_code == 401
-    #     assert response.json()["success"] is False
-    #     assert response.json()["message"] == "User not found"
+    def test_login_reviewer_internal_failed(self):
+        response = client.post(
+        "/api/auth",
+        data={"username": "reviewerinternalnonexist", "password": "wrongpassword"}
+    )
+        assert response.status_code == 401
+        assert response.json()["success"] is False
+        assert response.json()["message"] == "User not found"
 
-    # def test_login_reviewer_external(self):
-    #     response = client.post(
-    #     "/api/auth",
-    #     data={"username": "reviewer", "password": "reviewer"}
-    # )
-    #     assert response.status_code == 200
-    #     assert response.json()["success"] is True
-    #     assert "access_token" in response.json()["data"]
+    def test_login_reviewer_external(self):
+        response = client.post(
+        "/api/auth",
+        data={"username": "reviewer", "password": "reviewer"}
+    )
+        assert response.status_code == 200
+        assert response.json()["success"] is True
+        assert "access_token" in response.json()["data"]
 
-    # def test_login_reviewer_external_failed(self):
-    #     response = client.post(
-    #     "/api/auth",
-    #     data={"username": "reviewerexternalnonexist", "password": "wrongpassword"}
-    # )
-    #     assert response.status_code == 401
-    #     assert response.json()["success"] is False
-    #     assert response.json()["message"] == "User not found"
+    def test_login_reviewer_external_failed(self):
+        response = client.post(
+        "/api/auth",
+        data={"username": "reviewerexternalnonexist", "password": "wrongpassword"}
+    )
+        assert response.status_code == 401
+        assert response.json()["success"] is False
+        assert response.json()["message"] == "User not found"
         
 if __name__ == "__main__":
     runner = HTMLTestRunner(
